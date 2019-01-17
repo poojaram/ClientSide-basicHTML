e4d9567b43efd0c566b7d5d9443cb50c7debd8d9
Joel Ross <joelross@uw.edu>
2019-01-17 13:29:06 -0800

    Adjust linting rules

--- problem-a/problem.spec.js
+++ problem-a/problem.spec.js
@@ -31,6 +31,7 @@ describe('Source code is valid', () => {
       'line-end-style':false, //either way
       'indent-style':false, //can mix/match
       'indent-width':false, //don't need to beautify
+      'line-no-trailing-whitespace': false, //don't need to beautify
       'id-class-style':false, //I like dashes in classnames
       'img-req-alt':false, //for this test; captured later!
     }
